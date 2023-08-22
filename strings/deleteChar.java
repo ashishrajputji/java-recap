0943ae654466a8b4a794660e917b4f59c6338cda
ashish rajput <ashish.0017.rajput@gmail.com>
2023-08-22 22:56:23 +0530

    Day-7 commit 2

---
+++ strings/deleteChar.java
@@ -0,0 +1,20 @@
+package strings;
+import java.util.*;
+public class deleteChar {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a String");
+        StringBuilder sb = new StringBuilder(scan.nextLine());
+        char ch;
+        System.out.println("Enter character to delete from the string");
+        ch = scan.next().charAt(0);
+        scan.close();
+        for(int i=0;i<sb.length()-1;i++){
+            if(sb.charAt(i)==ch){
+                sb.deleteCharAt(i);
+            }
+        }
+        System.out.println(sb);
+
+    }
+}
