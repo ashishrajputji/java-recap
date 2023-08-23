31736a81bb9c5b6611948187378c802e314590e4
ashish rajput <ashish.0017.rajput@gmail.com>
2023-08-23 17:10:13 +0530

    Day 8 polymorphism done

---
+++ oops/MyPolymorphism.java
@@ -0,0 +1,22 @@
+package oops;
+class FirstPolymorphism{
+    String name;
+    int age;
+    public void printName(){
+        System.out.println("Method overloading to perform polymorphism");
+    }
+    public void printName(String name, int age){
+        System.out.print("Name is "+name+" and age is "+age);
+    }
+    FirstPolymorphism(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+}
+public class MyPolymorphism {
+    public static void main(String[] args) {
+        FirstPolymorphism fp = new FirstPolymorphism("Ashish Rajput", 18);
+        fp.printName();
+        fp.printName("Khushi",21);
+    }
+}
