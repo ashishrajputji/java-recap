0be50cc7c97739c89700908fb549354764fde95d
ashish rajput <ashish.0017.rajput@gmail.com>
2023-08-25 17:04:29 +0530

    Linked List first commit

---
+++ linkedList/AddPrintLL.java
@@ -0,0 +1,16 @@
+package linkedList;
+
+public class AddPrintLL {
+    class Node{
+        String data;
+        Node next;
+
+        Node(String data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
