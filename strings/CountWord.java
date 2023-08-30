cf12957bd687809e54637113097c4f5976048679
ashish rajput <ashish.0017.rajput@gmail.com>
2023-08-30 16:03:05 +0530

    count word

--- strings/CountWord.java
+++ strings/CountWord.java
@@ -2,6 +2,7 @@ package strings;
 import java.util.Scanner;
 public class CountWord {
     static int count = 0;
+    static int check = -1;
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter string to cound number of words ");
@@ -9,8 +10,15 @@ public class CountWord {
         // String sb = scan.nextLine();
         scan.close();
         for(int i=0;i<sb.length();i++){
-            if(sb.charAt(i) == ' ' && sb.charAt(i+1) != ' '){
+            if(check == -1 && sb.charAt(i)!= ' ') {
+                check = 0;
+            } else if(check == -1 && sb.charAt(i) == ' ') {
+                continue;
+            }
+            if(i<sb.length()-1){
+             if(sb.charAt(i) == ' ' && sb.charAt(i+1) != ' '){
                 count++;
+            }
             }
         }
         ++count;
