798385689f2934b673561de79595d1199f47fd0b
ashish rajput <ashish.0017.rajput@gmail.com>
2023-09-05 18:23:28 +0530

    Linked List insertion

--- linkedList/InsertLinkedList.java
+++ linkedList/InsertLinkedList.java
@@ -18,7 +18,46 @@ class InsLL{
         size++;
     }
 
+    // Insert At end
+    public void insertLast(int val){
+        Node node = new Node(val);
+        if(tail == null){
+            insertFirst(val);
+            return;
+        }
+        tail.next = node;
+        tail = node;
+        size++;
+    }
+    // Insert at particular index
+    public void insertAtIndex(int val,int index){
+        if(index == 0){
+            insertFirst(val);
+            return;
+        }
+        if(index == size){
+            insertLast(val);
+            return;
+        }
+        Node temp = head;
+        for(int i=1;i<index;i++){
+            temp = temp.next;
+        }
+        Node node = new Node(val,temp.next);
+        temp.next = node;
+        size++;
+    }
 
+    // Display or print Linked List
+    public void display(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(" -> "+temp.value+" and its next is "+temp.next);
+            temp = temp.next;
+        }
+    }
+
+// Single node class
     private class Node{
         private int value;
         private Node next;
@@ -37,5 +76,12 @@ public class InsertLinkedList {
     public static void main(String[] args) {
         InsLL l = new InsLL();
         l.insertFirst(22);
+        l.insertFirst(2);
+        l.insertFirst(9);
+        l.insertLast(0);
+        l.insertLast(10);
+        l.insertAtIndex(222, 3);
+
+        l.display();
     }
 }
