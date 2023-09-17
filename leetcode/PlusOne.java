560c7073e1b19e262f7459bdf04e0290d2d05e59
ashish rajput <ashish.0017.rajput@gmail.com>
2023-09-17 14:09:41 +0530

    Leetcode 17-09-2023  PlusOne

---
+++ leetcode/PlusOne.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class PlusOne {
+    static public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
+        }
+    
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+        }
+        public static void main(String[] args) {
+            int arr[] = {1,2,3,9};
+           System.out.println(Arrays.toString(plusOne(arr)));
+        }
+}
